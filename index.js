0e6fbc303e605872f7d1d10257301d45004eeb3f
unknown <tracergreen@gmail.com>
2020-09-24 21:37:46 -0600

    finished project, no stretch yet

--- index.js
+++ index.js
@@ -91,9 +91,12 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
-  } 
+
+function getLastReview(arr, index) {
+  let lastReview = arr[index];
+   return `${lastReview.name} gave the restaurant a ${lastReview.rating}, and their feedback was: ${lastReview.feedback}`
+ } 
+getLastReview(reviews, [reviews.length - 1]);
 
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
