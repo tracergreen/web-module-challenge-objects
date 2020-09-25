c7dc1cbedd25a39b5d9e948b2da0f10ec285f0c0
unknown <tracergreen@gmail.com>
2020-09-24 20:56:52 -0600

    all the way through task 6

--- index.js
+++ index.js
@@ -55,11 +55,17 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
+console.log(reviews[5].feedback);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+let newReview = {name: "Tracy", rating: 3.5, feedback: "I really enjoy this place, they have the best burgers in town!"};
+reviews.push(newReview);
+console.log(reviews);
 
+/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays" */
 
-/* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+reviews[7].feedback = 'This place is chill with really cool people, great for getting work done on weekdays!';
+console.log(reviews);
 
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
